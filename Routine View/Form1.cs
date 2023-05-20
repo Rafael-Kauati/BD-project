ee2244108a472684eabbd5606547e1a55b6b6d62
MiguelSoaresFrancisco <miguel_lamego@outlook.com>
2023-05-20 16:05:42 +0100

    login

--- Routine View/Form1.cs
+++ Routine View/Form1.cs
@@ -1,76 +1,62 @@
-using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace Routine_View
+namespace projeto_bd
 {
     public partial class Form1 : Form
     {
+        private SqlConnection cn;
+        private string connectionString = "data source=.\\;integrated security=true;initial catalog=Routine View";
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private SqlDataReader reader;
-        private SqlConnection cn;
-        private SqlCommand cmd;
-        private int currentContact;
-        private bool adding;
-
-        private SqlConnection getSGBDConnection()
-        {
-            return new SqlConnection("data source=.\\SQLEXPRESS;integrated security=true;initial catalog=Routine View");
-        }
-
-        private bool verifySGBDConnection()
-        {
-            if (cn == null)
-                cn = getSGBDConnection();
-
-            if (cn.State != ConnectionState.Open)
-                cn.Open();
-
-            return cn.State == ConnectionState.Open;
-        }
-
-        private void LoginHandler()
-        {
-            if (!verifySGBDConnection())
-                return;
-
-            //MessageBox.Show("CN sucess ! (1)");
-
-            //System.Console.WriteLine("CN sucess ! (2)");
-
-            String content = "";
-            cmd = new SqlCommand("SELECT * FROM [User]", cn);
-            reader = cmd.ExecuteReader();
-
-            while( reader.Read())
-            {
-                content += reader["Name"].ToString();
-            }
-            cn.Close();
-
-            MessageBox.Show(content);
-
-        }
-
-        String Email, Password;
-
-        private void LoginBtn_click(object sender, System.EventArgs e)
-        {
-            Email = EmailBox.Text;
-            Password = PasswordBox.Text;
-            LoginHandler();
+            cn = new SqlConnection(connectionString);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            /**/
-            cn = getSGBDConnection();
-            //MessageBox.Show("CN sucess ! (1)");
-            //System.Console.WriteLine("CN sucess ! (1)");
+            cn.Open();
+        }
+
+        private void LoginBtn_click(object sender, EventArgs e)
+        {
+            string email = EmailBox.Text;
+            string password = PasswordBox.Text;
+
+            if (VerifyLogin(email, password))
+            {
+                MessageBox.Show("Login successful!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid email or password!");
+            }
+        }
+
+        private bool VerifyLogin(string email, string password)
+        {
+            string query = "SELECT COUNT(*) FROM [User] WHERE Email = @Email AND Password = @Password";
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cn.Close();
         }
     }
 }
