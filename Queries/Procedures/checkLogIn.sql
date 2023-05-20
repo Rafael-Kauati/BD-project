87969d64fdcecf80a61e3b5e9e60323936c61283
Rafael-Kauati <64330066+Rafael-Kauati@users.noreply.github.com>
2023-05-20 19:03:07 +0100

    Login Procedure

---
+++ Queries/Procedures/checkLogIn.sql
@@ -0,0 +1,32 @@
+use [Routine View]
+go
+
+DROP PROCEDURE IF EXISTS checkLogIn;
+GO
+
+CREATE PROCEDURE checkLogIn
+   ( @email varchar(40),
+	@password varchar(40),
+	 @confirmation int OUTPUT)
+AS
+BEGIN
+	declare @matches  int ;
+	select @matches =  count(*) from [User] 
+	where [User].Email = @email and [User].[Password] = @password;
+	
+	IF @matches = 1
+		set @confirmation = 1;
+	ELSE
+		set @confirmation = 0;
+
+	return @confirmation;
+		
+END
+GO
+
+DECLARE @confirmation int;
+
+EXEC checkLogIn @email = 'JSP@ua.pt', @password = 'jayjay', @confirmation = @confirmation OUTPUT;
+
+PRINT @confirmation;
+
