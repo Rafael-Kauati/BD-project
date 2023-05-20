bf83d52205136b453e0fbd297e2c1550b925a1e5
Rafael-Kauati <64330066+Rafael-Kauati@users.noreply.github.com>
2023-05-20 15:15:24 +0100

    ++

--- Queries/Insertion.sql
+++ Queries/Insertion.sql
@@ -18,13 +18,49 @@ insert into
 	( 'To Do'), ( 'Doing'),( 'Done');
 go
 
-INSERT INTO [Task] (Title, Description, Importance, Deadline, Priority, StackID, UserID)
-VALUES 
-('Responder e-mails', 'Responder aos e-mails recebidos hoje', 2, '2023-05-13 17:00:00', 1, 1, 10),
-('Enviar convites', 'Enviar convites para o evento de lançamento do produto Y', 3, '2023-05-20 18:00:00', 2, 1, 10),
-('Analisar relatório', 'Analisar o relatório de vendas do último trimestre', 2, '2023-05-18 10:00:00', 1, 1, 10),
-('Conferir estoque', 'Conferir o estoque de produtos para o próximo mês', 3, '2023-05-19 11:00:00', 2, 1, 10);
-GO
+INSERT INTO [Task] (Title, [Description], Importance, Deadline, [Priority], [StackID], [UserID])
+VALUES ('Enviar relatório semanal', 'Preparar e enviar relatório de desempenho semanal da equipe', 2, '2023-05-19 09:00:00', 3, 1, 10),
+
+('Atualizar planilha de vendas', 'Inserir os dados de vendas atualizados na planilha de acompanhamento', 1, '2023-05-20 14:30:00', 2, 2, 10),
+ ('Realizar reunião de equipe', 'Agendar e conduzir uma reunião com a equipe para alinhar as metas do mês', 3, '2023-05-21 11:00:00', 1, 1, 10),
+
+ ('Enviar convites para evento', 'Elaborar e enviar convites para os participantes do evento corporativo', 2, '2023-05-22 16:30:00', 2, 2, 10),
+
+ ('Preparar apresentação de vendas', 'Criar uma apresentação persuasiva para a reunião com potenciais clientes', 1, '2023-05-23 13:00:00', 1, 1, 10),
+
+ ('Realizar análise de dados', 'Analisar os dados de vendas do último trimestre e identificar padrões', 3, '2023-05-24 10:30:00', 2, 2, 10),
+
+ ('Enviar proposta comercial', 'Elaborar e enviar a proposta comercial para o cliente em potencial', 2, '2023-05-25 15:00:00', 3, 1, 10),
+
+ ('Revisar contrato de fornecedor', 'Analisar e revisar o contrato de fornecimento antes de assinar', 1, '2023-05-26 12:30:00', 2, 2, 10),
+
+ ('Realizar teste de software', 'Executar testes e identificar possíveis bugs no novo software', 3, '2023-05-27 09:00:00', 1, 1, 10),
+
+ ('Organizar treinamento interno', 'Agendar e organizar um treinamento para atualizar os colaboradores', 2, '2023-05-28 14:30:00', 2, 2, 10),
+
+ ('Fazer backup dos arquivos', 'Realizar o backup dos arquivos importantes para evitar perda de dados', 1, '2023-05-29 11:00:00', 1, 1, 10),
+
+ ('Atualizar redes sociais', 'Postar conteúdo atualizado nas redes sociais da empresa', 3, '2023-05-30 16:30:00', 2, 2, 10),
+
+ ('Realizar pesquisa de mercado', 'Conduzir uma pesquisa de mercado para identificar novas oportunidades', 2, '2023-05-31 13:00:00', 1, 1, 10),
+
+ ('Preparar relatório financeiro', 'Analisar as finanças da empresa e preparar um relatório detalhado', 1, '2023-06-01 10:30:00', 2, 2, 10),
+
+ ('Enviar lembrete de pagamento', 'Enviar lembretes de pagamento aos clientes com faturas em atraso', 3, '2023-06-02 15:00:00', 3, 1, 10),
+
+ ('Desenvolver novo produto', 'Iniciar o desenvolvimento de um novo produto inovador para o mercado', 2, '2023-06-03 12:30:00', 2, 2, 10),
+
+ ('Realizar auditoria interna', 'Conduzir uma auditoria interna para garantir conformidade com os padrões', 1, '2023-06-04 09:00:00', 1, 1, 10),
+
+ ('Atualizar sistema de gestão', 'Implementar atualizações no sistema de gestão da empresa', 3, '2023-06-05 14:30:00', 2, 2, 10),
+
+ ('Realizar pesquisa de satisfação', 'Enviar pesquisa de satisfação aos clientes e analisar os resultados', 2, '2023-06-06 11:00:00', 1, 1, 10),
+
+ ('Preparar material de treinamento', 'Desenvolver materiais de treinamento para a equipe de vendas', 1, '2023-06-07 16:30:00', 2, 2, 10),
+
+ ('Analisar concorrência', 'Realizar uma análise detalhada dos principais concorrentes no mercado', 3, '2023-06-08 13:00:00', 1, 1, 10),
+
+ ('Enviar relatório trimestral', 'Preparar e enviar o relatório de desempenho trimestral da empresa', 2, '2023-06-09 10:30:00', 2, 2, 10);
 
 UPDATE [Task] 
 SET StackID = 3 
