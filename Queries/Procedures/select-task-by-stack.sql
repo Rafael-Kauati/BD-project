23736273a016bae3414b2e8730e177b07d4898d5
Rafael-Kauati <64330066+Rafael-Kauati@users.noreply.github.com>
2023-05-20 15:19:06 +0100

    procedurer n. 1

---
+++ Queries/Procedures/select-task-by-stack.sql
@@ -0,0 +1,8 @@
+create procedure selectTasksByStack( @stackname  varchar(10) )
+as
+	select Code, Title, [Description] 
+	from Task 
+	inner join Stack on Task.StackID = Stack.StackID
+	where Stack.Name = @stackname;
+
+exec selectTasksByStack 'Doing';
