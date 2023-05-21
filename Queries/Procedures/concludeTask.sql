8ec7ea21b26483ec2d2df4518113a410c93497ec
Rafael-Kauati <64330066+Rafael-Kauati@users.noreply.github.com>
2023-05-21 17:05:14 +0100

    Procedure de conclusão da task que cria um reward

--- Queries/Procedures/concludeTask.sql
+++ Queries/Procedures/concludeTask.sql
@@ -1,20 +1,39 @@
-use [Routine View]
-go
+USE [Routine View]
+GO
 
-drop procedure if exists concludeTask
-go
+DROP PROCEDURE IF EXISTS concludeTask
+GO
 
-create procedure concludeTask(@taskname varchar(30))
-as
-begin
-	update [Task]
-	set StackID = 3
-	where Task.Title = @taskname;
+CREATE PROCEDURE concludeTask (@taskname VARCHAR(30))
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION; -- Inicia a transação
 
-	select * from [Stack]; 
-	select * from [Task]; 
-end
-go
-exec concludeTask 'Enviar relatório semanal';
+        DECLARE @taskcode INT;
+        DECLARE @taskdeadline DATETIME;
+        DECLARE @taskpriority INT;
 
+        SELECT @taskcode = Code, @taskdeadline = Deadline, @taskpriority = [Priority]
+        FROM [Task]
+        WHERE Title = @taskname;
+
+        UPDATE [Task]
+        SET StackID = 3
+        WHERE Title = @taskname;
+
+        INSERT INTO [Reward] ([Task_code], [Task_Deadline], [Date_Time], [Reward_Value])
+        VALUES (@taskcode, ISNULL(@taskdeadline, GETDATE()), GETDATE(), @taskpriority * 2);
+
+        COMMIT; 
+
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK; 
+    END CATCH
+END
+GO
+
+EXEC concludeTask 'Conferir estoque';
 
---
+++ Queries/Triggers/updateUserScore.sql
@@ -0,0 +1,25 @@
+use [Routine View]
+go
+
+drop trigger if exists updateUserScore
+go
+
+create trigger updateUserScore on [Reward] 
+after insert 
+as 
+begin
+	declare @newReward int; declare @userid int;
+	
+	select * from [User];
+
+	select 
+	@newReward = [Reward_Value],
+	@userid = Task.UserID
+	from inserted 
+	inner join Task on inserted.Task_code = Task.Code;
+
+	update [User]
+	set [Score] = [Score] + @newReward
+	where [User].ID = @userid;
+	
+end;
